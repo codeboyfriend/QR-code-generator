6fe7bfd8d0b3853e6af5c1e3729469f90517b2ad
codeboyfriend <mkinggrafix@gmail.com>
2022-12-26 02:45:38 -0800

    Generate the QR Code UI

--- src/script.js
+++ src/script.js
@@ -1,18 +1,40 @@
 let form = document.getElementById('generate-form');
-let url = document.getElementById('url');
-let qr = document.getElementById('qr-code')
+let qr = document.getElementById('qrcode');
+let spinner = document.getElementById('spinner');
+let body = document.getElementById('body');
 
 const onGenerate = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    let qrcode = new QRCode("qr-code", {
-        text: url.value,
-        width: 128,
-        height: 128,
-        colorDark : "#000000",
-        colorLight : "#ffffff",
-        correctLevel : QRCode.CorrectLevel.H
-    });
+    clearUI();
+    const url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
+
+    if (url === "") {
+        console.log('Please')
+    } else {
+        spinner.style.display = 'flex';
+        body.style.overflowY = 'hidden';
+
+        setTimeout(() => {
+            spinner.style.display = 'none';
+            body.style.overflowY = 'scroll';
+            generateQR(url, size);
+        }, 1000);
+    }
+    
+}
+
+const generateQR = (url, size) => {
+    const qrcode = new QRCode(qr, {
+        text: url,
+        width: size,
+        height: size,
+    })
+}
+
+const clearUI = () => {
+    qr.innerText = '';
 }
 
 
